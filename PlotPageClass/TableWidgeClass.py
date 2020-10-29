786c90fa778cc90106318b4bb24c5f494b84edfa
alterChiu <andy950702@gmail.com>
2020-10-29 10:48:55 +0800

    20201029

--- PlotPageClass/TableWidgeClass.py
+++ PlotPageClass/TableWidgeClass.py
@@ -4,10 +4,11 @@ from PyQt5.QtCore import Qt
 import statistics
 import traceback
 import sys
+import math
 
 class TableWidgeClass:
 
-    __tableData = [] #[[x,y,l,z] , [x,y,l,z].....]
+   
 
     def __init__(self , tableWidget:QTableWidget):
 
@@ -15,6 +16,7 @@ class TableWidgeClass:
         # set table
         #----------------------------
         self.__tableWidget = tableWidget
+        self.__tableData = [] #[[x,y,l,z] , [x,y,l,z].....]
         
         #self.__tableWidget.horizontalHeader().setSectionResizeMode(5,QHeaderView.Stretch)# set 6 column autoSize
         self.__tableWidget.horizontalHeader().setSectionsClickable(False) # unable click title
@@ -36,7 +38,7 @@ class TableWidgeClass:
         self.__startX = None
         self.__startY = None
         self.__startZ = None
-        __tableData = []
+        self.__tableData = []
         self.__startX = x
         self.__startY = y
         self.__startZ = z
@@ -52,7 +54,7 @@ class TableWidgeClass:
         self.__endX = None
         self.__endY = None
         self.__endZ = None
-        __tableData = []
+        self.__tableData = []
         self.__endX = x
         self.__endY = y
         self.__endZ = z
@@ -74,15 +76,12 @@ class TableWidgeClass:
                 temptY = temptXY[1]
 
                 # add to collection
-                __tableData.append([temptX , temptY , l , z])
+                self.__tableData.append([temptX , temptY , l , z])
+                self.__tableData.sort(key = lambda s:s[2])
 
-                __tableData.sort(key = lambda s:s[2])
+                # reload
+                self.__reload()
 
-                # add to table
-                self.__addLine(temptX , temptY , l ,z)
-
-                # sorted
-                self.__tableWidget.sortByColumn(2)
             else:
                 print("invalid point")
         except Exception:
@@ -93,7 +92,7 @@ class TableWidgeClass:
         
     def deletPoint(self , index:int):
         try:
-            __tableData.pop(index)
+            self.__tableData.pop(index)
         except:
             pass
     
@@ -162,9 +161,9 @@ class TableWidgeClass:
     def __tableDataMoveL(self , moveL:float , range:int): # range <0:left , >0:right
 
         # detect by range
-        for row in range(0,len(__tableData)):
-            if __tableData[row][3]*range > 0:
-                temptL = __tableData[row][2] + moveL
+        for row in range(0,len(self.__tableData)):
+            if self.__tableData[row][3]*range > 0:
+                temptL = self.__tableData[row][2] + moveL
 
                 # change value then reload selected row    
                 if math.abs(temptL) < self.__totalLength/2 and temptL*range>0:
@@ -176,9 +175,9 @@ class TableWidgeClass:
     def __tableDataMoveZ(self , moveZ:float , range:int):# range <0:left , >0:right
         
         # detect by range
-        for row in range(0,len(__tableData)):
-            if __tableData[row][3]*range > 0:
-                temptZ = __tableData[row][3] + moveZ
+        for row in range(0,len(self.__tableData)):
+            if self.__tableData[row][3]*range > 0:
+                temptZ = self.__tableData[row][3] + moveZ
                 self.setValue(row,3,temptZ)
 
 
@@ -186,18 +185,18 @@ class TableWidgeClass:
     #----------------------------------------------------------
     def __addLine(self , x:float , y:float , l:float , z:float):
         row = self.__tableWidget.rowCount()
-        self.__tableWidget.setRowCount(row+1)
+        self.__tableWidget.insertRow(row)
 
-        self.__tableWidget.setItem(row , 0 , QTableWidgetItem(x))
-        self.__tableWidget.setItem(row , 1 , QTableWidgetItem(y))
-        self.__tableWidget.setItem(row , 2 , QTableWidgetItem(l))
-        self.__tableWidget.setItem(row , 3 , QTableWidgetItem(z))
+        self.__tableWidget.setItem(row , 0 , QTableWidgetItem(str(x)))
+        self.__tableWidget.setItem(row , 1 , QTableWidgetItem(str(y)))
+        self.__tableWidget.setItem(row , 2 , QTableWidgetItem(str(l)))
+        self.__tableWidget.setItem(row , 3 , QTableWidgetItem(str(z)))
 
         self.__tableWidget.item(row,0).setFlags(Qt.ItemIsEditable)
         self.__tableWidget.item(row,1).setFlags(Qt.ItemIsEditable)
 
-    def __reload(self , tableData:list=__tableData):
-        self.__tableWidget.clearContents()
+    def __reload(self):
+        self.__tableWidget.setRowCount(0)
 
         # add startPoint
         #self.__addLine(self.__startX , self.__startY , 0 , self.__startZ)
@@ -206,14 +205,13 @@ class TableWidgeClass:
         #self.__addLine(self.__endX , self.__endY , self.__totalLength , self.__endZ)
 
         # add other point
-        for point in tableData:
-             self.__addLine(point[0] , point[1] , point[2] , point[3])
+        for index in range(-1 , len(self.__tableData)*-1-1 , -1):
+            point = self.__tableData[index]
+            self.__addLine(point[0] , point[1] , point[2] , point[3])
 
-        # sorted
-        self.__tableWidget.sortByColumn(2)
 
     def __reloadRow(self,row:int):
-        temptL = __tableData[row][2]
+        temptL = self.__tableData[row][2]
         temptXY = self.__lengthToXY(temptL)
         self.__tableWidget.setItem(row , 0 , QTableWidgetItem(temptXY[0]))
         self.__tableWidget.setItem(row , 1 , QTableWidgetItem(temptXY[1]))
